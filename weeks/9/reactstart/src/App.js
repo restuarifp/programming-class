2fa1957852659d488daefe72dcf1521b8a67f019
Flameg0d <agnipamungkas@gmail.com>
2025-05-03 13:43:16 +0700

    Add files via upload

---
+++ weeks/9/reactstart/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Images = [
+'https://assets.pikiran-rakyat.com/crop/0x131:1027x817/157x94/webp/photo/2022/09/25/1515576310.jpg'
+]
+
+  
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {Images.map((img)=> {
+          return <img src={img} className="App-logo" alt="logo" />})}
+        
+        <p>
+          Programming 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
